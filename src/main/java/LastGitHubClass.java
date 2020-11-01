ddfd16920896693ecf3da985feca8c8e6b0b23fc
altayozyavas <altayozyavas@gmail.com>
2020-11-01 15:48:05 -0600

    name fixed

--- src/main/java/LastGitHubClass.java
+++ src/main/java/LastGitHubClass.java
@@ -1,4 +1,4 @@
-public class LastGutHubClass {
+public class LastGitHubClass {
 
     public static void main(String[] args) {
         System.out.println("I have learned a lot about Git and GitHub ");
